eeea86366b8c0fbab61e2e11bdef2265b7b935e5
bigbaldy <wjz@163.com>
2017-03-16 11:16:53 +0800

    add MaxSubArray

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,12 @@
+import algorithms.MaxSubArray;
+
 /**
  * Created by wangjinzhao on 2017/3/16.
  */
 public class Test {
     public static void main(String... args)
     {
-
+        int res= new MaxSubArray().maxSubArray(new int[]{-2,-1});
+        System.out.println(res);
     }
 }
